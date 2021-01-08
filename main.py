f4aaa65f9fa23d523cd05ce3098ebc45f4a48d0e
wojciech-bigosinski <wojciechbigosinski@gmail.com>
2021-01-08 19:21:02 +0100

    placeholder functions

--- main.py
+++ main.py
@@ -13,6 +13,33 @@ def login():
 def register():
     return "success", 200
 
+@app.route('/poll/', methods=['GET', 'POST'])
+def commitPoll():
+    return "success", 200
+
+@app.route('/changeUser/', methods=['GET', 'POST'])
+def changeUser():
+    return "success", 200
+
+@app.route('/banUser/', methods=['GET', 'POST'])
+def banUser():
+    return "success", 200
+
+@app.route('/changeReward/', methods=['GET', 'POST'])
+def changeReward():
+    return "success", 200
+
+@app.route('/addReward/', methods=['GET', 'POST'])
+def addReward():
+    return "success", 200
+
+@app.route('/deleteReward/', methods=['GET', 'POST'])
+def deleteReward():
+    return "success", 200
+
+@app.route('/claimReward/', methods=['GET', 'POST'])
+def claimReward():
+    return "success", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
